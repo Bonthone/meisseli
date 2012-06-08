5ffeaef335075840c105372531231d7f553f8116
Emil S <emil.sagfors@aalto.fi>
2012-06-09 01:14:53 +0300

    use data_mapper for sqlite/postgresql

--- Gemfile
+++ Gemfile
@@ -8,4 +8,5 @@ end
 
 group :production do
 	gem 'pg', '0.10.0' # this gem is required to use postgresql in heroku
+	gem 'dm-postgres-adapter'
 end
--- server.rb
+++ server.rb
@@ -1,5 +1,8 @@
 class Meisseli < Sinatra::Base
 
+	# Setting up the database
+	DataMapper.setup(:default, ENV['DATABASE_URL'] || "sqlite3://#{Dir.pwd}/mydatabase.db")
+
 	get '/' do
 		return 'Hello World!'
 	end
