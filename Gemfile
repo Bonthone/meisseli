8460532c3e0111b1ed2aceabb878922b29704cac
Emil S <emil.sagfors@aalto.fi>
2012-06-09 04:51:33 +0300

    changed the whole setup to MySQL

--- Gemfile
+++ Gemfile
@@ -1,12 +1,7 @@
 source 'http://rubygems.org'
 gem 'sinatra', :require => 'sinatra/base'
-gem 'data_mapper'
+gem 'json'
 
-group :development, :test do 
-	gem 'sqlite3'
-end
-
-group :production do
-	gem 'pg', '0.10.0' # this gem is required to use postgresql in heroku
-	gem 'dm-postgres-adapter'
-end
+#MySQL
+gem 'mysql2'
+gem 'activerecord', :require => 'active_record'
--- server.rb
+++ server.rb
@@ -1,9 +1,49 @@
 class Meisseli < Sinatra::Base
 
+	class MyAPI < ActiveRecord::Base
+		class << self
+
+			def get_users()
+				query='select * from tblUser';
+				self.connection.select_all(query).to_json
+			end
+
+		end
+	end
+
 	# Setting up the database
-	DataMapper.setup(:default, ENV['DATABASE_URL'] || "sqlite3://#{Dir.pwd}/mydatabase.db")
+	dbconfig = {}
+
+	configure :development do
+		dbconfig = {
+			:adapter => "mysql2",
+			:host => "localhost",
+			:username => "dibs",
+			:password => "peruna",
+			:database => "dibs"
+		}
+	end
+
+	configure :production do
+		dbconfig = {
+			:adapter => "mysql2",
+			:host => "us-cdbr-east.cleardb.com",
+			:username => "ad56145265828d",
+			:password => "8b97f200",
+			:database => "heroku_f3199d24cd86b48"
+		}
+	end
+
+	# Connect to database
+	MyAPI.establish_connection(dbconfig)
+
 
 	get '/' do
-		return 'Hello World!'
+		"Hello world, it's #{Time.now} at the server!"
 	end
+
+	get '/users/' do
+		MyAPI.get_users()
+	end
+
 end
